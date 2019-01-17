a9864fca6fb4beb3cbeeef69136465ce6fc2d18d
Jake Mattson <JakeJMattson@gmail.com>
2019-01-17 16:18:05 -0600

    Removed unused configuration fields

--- src/main/kotlin/io/github/jakejmattson/wiretap/services/Configuration.kt
+++ src/main/kotlin/io/github/jakejmattson/wiretap/services/Configuration.kt
@@ -3,9 +3,7 @@ package io.github.jakejmattson.wiretap.services
 import me.aberrantfox.kjdautils.api.annotation.Data
 
 @Data("config/config.json")
-data class Configuration(val token: String = "insert-token-here",
-						 val prefix: String = "?",
-						 val watchCategory: String = "insert-id-here",
+data class Configuration(val watchCategory: String = "insert-id-here",
 						 val wordLogChannel: String = "insert-id-here",
 						 val requiredRoleName: String = "Staff",
 						 val recoverWatched: Boolean = true)
