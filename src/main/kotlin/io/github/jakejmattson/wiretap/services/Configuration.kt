3f28b5c289b1df1af560905673bf71b514d1902f
Jake Mattson <JakeJMattson@gmail.com>
2019-09-04 23:38:31 -0500

    Remove old configuration entities

--- src/main/kotlin/io/github/jakejmattson/wiretap/services/Configuration.kt
+++ src/main/kotlin/io/github/jakejmattson/wiretap/services/Configuration.kt
@@ -3,8 +3,7 @@ package io.github.jakejmattson.wiretap.services
 import me.aberrantfox.kjdautils.api.annotation.Data
 
 @Data("config/config.json")
-data class Configuration(val generateDocsAtRuntime: Boolean = false,
-                         val watchCategory: String = "insert-id-here",
+data class Configuration(val watchCategory: String = "insert-id-here",
                          val wordLogChannel: String = "insert-id-here",
                          val requiredRoleName: String = "Staff",
                          val recoverWatched: Boolean = true)
