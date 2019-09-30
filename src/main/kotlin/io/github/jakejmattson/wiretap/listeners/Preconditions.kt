885b5444a40b6c3408419ed5be5db5b9a80e846c
Jake Mattson <JakeJMattson@gmail.com>
2019-09-29 23:41:30 -0500

    Update preconditions

--- src/main/kotlin/io/github/jakejmattson/wiretap/listeners/Preconditions.kt
+++ src/main/kotlin/io/github/jakejmattson/wiretap/listeners/Preconditions.kt
@@ -1,21 +1,19 @@
 package io.github.jakejmattson.wiretap.listeners
 
 import io.github.jakejmattson.wiretap.services.Configuration
-import me.aberrantfox.kjdautils.api.dsl.CommandEvent
-import me.aberrantfox.kjdautils.api.dsl.Precondition
+import me.aberrantfox.kjdautils.api.dsl.*
 import me.aberrantfox.kjdautils.internal.command.*
 
 @Precondition
-fun rolePrecondition(config: Configuration)  = exit@ { event: CommandEvent ->
-
+fun rolePrecondition(config: Configuration)  = precondition { event: CommandEvent ->
 	val guild = event.message.guild
 	val requiredRole = guild.getRolesByName(config.requiredRoleName, true).firstOrNull()
 	val memberRoles = guild.getMember(event.author)!!.roles
 
-	requiredRole ?: return@exit Fail("Required role (${config.requiredRoleName}) in config not found in guild!")
+	requiredRole ?: return@precondition Fail("Required role (${config.requiredRoleName}) in config not found in guild!")
 
 	if (requiredRole !in memberRoles)
-		return@exit Fail("You do not have the required role: ${requiredRole.name}")
+		return@precondition Fail("You do not have the required role: ${requiredRole.name}")
 
-	return@exit Pass
+	return@precondition Pass
 }
