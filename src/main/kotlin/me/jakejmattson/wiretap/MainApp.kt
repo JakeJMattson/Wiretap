8131df6eef0ac33eee341d0b117304b6dca7ff42
Jake Mattson <JakeJMattson@gmail.com>
2020-11-17 00:32:57 -0600

    Move StartupService logic to main

--- src/main/kotlin/me/jakejmattson/wiretap/MainApp.kt
+++ src/main/kotlin/me/jakejmattson/wiretap/MainApp.kt
@@ -1,11 +1,38 @@
 package me.jakejmattson.wiretap
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.*
 import me.jakejmattson.discordkt.api.dsl.bot
+import me.jakejmattson.discordkt.api.extensions.*
+import java.awt.Color
 
-fun main(args: Array<String>) {
+@Serializable
+private data class Properties(val version: String, val author: String, val repository: String)
+
+private val propFile = Properties::class.java.getResource("/properties.json").readText()
+private val project = Json.decodeFromJsonElement<Properties>(propFile)
+
+suspend fun main(args: Array<String>) {
     val token = args.first()
 
     bot(token) {
+        prefix { "?" }
 
+        mentionEmbed {
+            val self = it.discord.api.getSelf()
+
+            color = Color(0x00bfff)
+
+            thumbnail {
+                url = self.avatar.url
+            }
+
+            addField(self.tag, "The friendly eavesdropping bot.")
+            addInlineField("Required role", configuration.requiredRoleName)
+            addInlineField("Prefix", "?")
+            addInlineField("Author", "[${project.author}](https://discordapp.com/users/254786431656919051/)")
+            addInlineField("Version", project.version)
+            addInlineField("Source", project.repository)
+        }
     }
 }
